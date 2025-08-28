33070c1332ce4e631997db0f7890f14f8931730b
Douglas Rodrigues <163287204+Douglas-Luiz-de-Oliveira-Rodrigues@users.noreply.github.com>
2025-08-28 19:02:37 -0300

    desafio_04
    Sistema para uma loja de R$1.99, usando o for.

---
+++ desafio_04/app.js
@@ -0,0 +1,8 @@
+/* Faça um programa que exiba essa tabela conforme
+o exemplo, no console do browser.
+*/
+
+console.log("LOJA QUASE DOIS - TABELA DE PREÇOS");
+for(let i = 1; i <= 50; i++){
+    console.log(`${i} - R$ ${i * 1.99}`);
+}
