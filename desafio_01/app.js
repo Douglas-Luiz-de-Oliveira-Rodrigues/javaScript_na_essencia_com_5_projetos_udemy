6d6938f132519be1b6ee8de2d58954807bef9f67
Douglas Rodrigues <163287204+Douglas-Luiz-de-Oliveira-Rodrigues@users.noreply.github.com>
2025-08-28 18:29:44 -0300

    desafio_01
    Sistema de supermercado, usando string, number e boolean.

---
+++ desafio_01/app.js
@@ -0,0 +1,20 @@
+/* Desafio
+
+    Faça um programa que exibe no console do browser
+    três informações sobre determinado produto.
+
+    As informações são: nome, preço e se está ou não no prazo de validade.
+
+    . String
+    . Number
+    . Boolean
+
+*/
+
+let nomeProduto = "Toddy";
+let precoProduto = 6.99;
+let validadeProduto = true;
+
+console.log(nomeProduto);
+console.log(precoProduto);
+console.log(validadeProduto);
