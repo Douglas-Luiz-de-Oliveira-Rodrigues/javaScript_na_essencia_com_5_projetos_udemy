3352b71c487acfd2d12b236c0814a144cdd48bd0
Douglas Rodrigues <163287204+Douglas-Luiz-de-Oliveira-Rodrigues@users.noreply.github.com>
2025-08-28 18:34:12 -0300

    desafio_03
    Sistema para provas de atletismo, usando if, else e else if.

---
+++ desafio_03/app.js
@@ -0,0 +1,17 @@
+/* Desafio Sistema de provas de atletismo */
+
+/* Faça um programa que receba três valores de tempo gasto,
+referentes a cada corredor, para realizar a prova e exiba no browser qual deles foi o vencedor.
+*/
+
+let tempo1 = 10;
+let tempo2 = 11;
+let tempo3 = 9;
+
+if(tempo1 < tempo2 && tempo1 < tempo3){
+    console.log("Corredor 1 foi o vencedor!!!")
+}else if(tempo2 < tempo1 && tempo2 < tempo3){
+    console.log("Corredor 2 foi o vencedor!!!")
+}else{
+    console.log("Corredor 3 foi o vencedor!!!")
+}
