eb362d24066fd4db7bac370a2d4fffbebd3c3602
Douglas Rodrigues <163287204+Douglas-Luiz-de-Oliveira-Rodrigues@users.noreply.github.com>
2025-08-28 18:31:43 -0300

    desafio_02
    Sistema de notas para escola, calculando a média.

---
+++ desafio_02/app.js
@@ -0,0 +1,13 @@
+/* Desafio 
+    Faça um sistema que receba 4 notas, uma por bimestre e
+    calcula a média dessas 4 notas.
+    A média deverá ser exibida no console do browser.
+*/
+
+let nota1 = 7;
+let nota2 = 8;
+let nota3 = 9;
+let nota4 = 8;
+let media = (nota1 + nota2 + nota3 + nota4) / 4; // 8
+
+console.log("A média é: ", media);
